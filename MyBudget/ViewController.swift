4686f8660455787ac4f286b24a01638036029f49
Orlando Vargas <OrlandoV87@live.com>
2019-06-11 22:47:10 -0400

    Added labels and textfields for the login page

--- MyBudget/ViewController.swift
+++ MyBudget/ViewController.swift
@@ -12,11 +12,40 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var signInButton: UIButton!
     @IBOutlet weak var whiteLayout: UIView!
+    @IBOutlet weak var imageViewIcon: UIImageView!
+    
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var passwordLabel: UILabel!
+    @IBOutlet weak var passwordTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         signInButton.layer.cornerRadius = 8
+        signInButton.layer.shadowColor = UIColor.black.cgColor
+        signInButton.layer.shadowOffset = CGSize(width: 2, height: 2)
+        signInButton.layer.shadowRadius = 3
+        signInButton.layer.shadowOpacity = 0.8
         whiteLayout.layer.cornerRadius = 8
+        
+        
+        imageViewIcon.layer.cornerRadius = 64
+        
     }
-
+    
+    @IBAction func signIn(_ sender: Any) {
+        
+        
+        UIView.animate(withDuration: 0.5) {
+            self.signInButton.isHidden = true
+            
+        }
+        
+        userNameLabel.isHidden = true
+        userNameTextField.isHidden = true
+        passwordLabel.isHidden = true
+        passwordTextField.isHidden = true
+        
+    }
+    
 }
